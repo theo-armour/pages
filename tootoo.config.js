413e730a15a1bb568ec61ba59903f27d342ac2d9
Theo Armour <t.armour@gmail.com>
2026-06-24 19:56:20 -0700

    Add faviconFile property to TOOTOO_CONFIG for favicon management

--- tootoo.config.js
+++ tootoo.config.js
@@ -8,6 +8,7 @@ window.TOOTOO_CONFIG = {
   subtitle: '',
   faviconLetters: 'PG',
   faviconColor: '#e67e22',
+  faviconFile: 'favicon.ico',
   subtitle: 'a GitHub repository browser',
   headingFontUrl: 'https://fonts.googleapis.com/css2?family=Patua+One&display=swap',
   headingFont: '"Patua One", serif',
